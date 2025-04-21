a8c2d5dfa9ae75c6a1fd8aaa813fbca6e8d69e00
mervet-othman <m.othman@proxiad.com>
2025-04-21 14:05:30 +0100

    Sauvegarde avant pull

--- src/main/java/org/example/kataoptionalstreams/NullPointerDemo/NullPointerDemo.java
+++ src/main/java/org/example/kataoptionalstreams/NullPointerDemo/NullPointerDemo.java
@@ -1,4 +1,23 @@
 package org.example.kataoptionalstreams.NullPointerDemo;
 
+
+import java.util.Optional;
+
 public class NullPointerDemo {
+    public static void main(String[] args) {
+
+        // === Cas classique avec null ===
+        String entreprise = null;
+
+        //  Provoque un NullPointerException
+        System.out.println("Nom en majuscules : " + entreprise.toUpperCase());
+
+        // === Correction avec Optional (à activer en live) ===
+        /*
+        Optional<String> entreprise = Optional.ofNullable(null);
+
+        String nom = entreprise.orElse("Valeur par défaut");
+        System.out.println(" Nom affiché avec Optional : " + nom.toUpperCase());
+        */
+    }
 }
